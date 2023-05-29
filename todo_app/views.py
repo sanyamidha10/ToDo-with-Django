745034b98bda942c89a25bbfdffff724f74cd8a5
Sanya Midha <sanyamidha.10@gmail.com>
2023-05-29 21:35:33 +0530

    Add task functionality

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Task
 
 # Create your views here.
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task = task)
+    
+    return redirect('home')
